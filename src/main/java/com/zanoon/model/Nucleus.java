d4af614265947004ea453ab512fedb145016da25
Al-Zaytoun <zanoonhassan6@gmail.com>
2025-12-31 04:37:26 -0700

    Included basic functionality for nucleus cluster logic, still bugged and will continue to work on this step

--- src/main/java/com/zanoon/model/Nucleus.java
+++ src/main/java/com/zanoon/model/Nucleus.java
@@ -6,38 +6,81 @@ import java.util.ArrayList;
 
 // ArrayList<Particles>
 public class Nucleus {
-    private int numOfNeutrons;
-    private int numOfProtons;
     private double cx; // Center x-coordinate
     private double cy; // Center y-coordinate
 
     private List<Particle> particles;
 
-    public Nucleus(int numOfNeutrons, int numOfProtons, double cx, double cy) {
-        this.numOfNeutrons = numOfNeutrons;
-        this.numOfProtons = numOfProtons;
+    public Nucleus(double cx, double cy) {
+
         this.cx = cx;
         this.cy = cy;
         this.particles = new ArrayList<>();
     }
 
-    // Function to randomly place particles around center point radius
-    private void initialPlacement(double cx, double cy) {
-
-        int maxRadiusPlacement = 20; // Arbitrary value to indicate how far from the center a particle can be placed at
+    private void randomParticlePlacement(double cx, double cy, int maxRadius) {
+        // Generate points within maxRadius
         Random rand = new Random();
 
         for (int i = 0; i < particles.size(); i++) {
             Particle particle = particles.get(i);
-
             
+            double a = rand.nextDouble() * (2 * Math.PI);
+            double r = particle.getR() * maxRadius;
+            double x = cx + r * Math.cos(a);
+            double y = cy + r * Math.sin(a);
+            
+            particle.setPosition(x, y);
+
         }
 
     }
 
+    // Function to organically cluster nucleon particles together, it follows a 2-step process of randomly generating particles within a radius, and handling collisions with every particle
+    private void collisionParticleResolution(double cx, double cy) {
 
-    public void update() {
-        initialPlacement(cx, cy);
+        double overlapMagnitutde = 0.05;
+
+        // Resolve collisions with every particle pair
+
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < particles.size(); j++) {
+                Particle a = particles.get(j);
+                for (int k = j + 1; k < particles.size(); k++) {
+                    Particle b = particles.get(k);
+
+                    double dx = a.getX() - b.getX();
+                    double dy = a.getY() - b.getY();
+
+                    double dist = Math.sqrt(dx * dx + dy * dy);
+                    double minDist = a.getR() + b.getR();
+
+                    if (dist < minDist && dist > 0) { // Overlapping occured, move both particles outwards based on the nx/ny directions
+                        double overlap = minDist - dist;
+                        double nx = dx / dist;
+                        double ny = dy / dist;
+
+                        a.setPosition(a.getX() + (nx * overlap * overlapMagnitutde), a.getY() + (ny * overlap * overlapMagnitutde)); // New position = old position + displacement
+                        b.setPosition(b.getX() - (nx * overlap * overlapMagnitutde), b.getY() - (ny * overlap * overlapMagnitutde));
+
+                    }
+                }
+            }
+        }
+
+    }
+
+    public void organicCluster(int maxRadius) {
+        randomParticlePlacement(cx, cy, maxRadius);
+        collisionParticleResolution(cx, cy);
+    }
+
+    public void addParticle(Particle p) {
+        particles.add(p);
+    }
+
+    public List<Particle> getParticles() {
+        return particles;
     }
 
 }
--- src/main/java/com/zanoon/render/SimulationPanel.java
+++ src/main/java/com/zanoon/render/SimulationPanel.java
@@ -2,31 +2,51 @@ package src.main.java.com.zanoon.render;
 
 import java.awt.Color;
 import java.awt.Graphics;
+
 import javax.swing.JPanel;
 import javax.swing.Timer;
-
 import src.main.java.com.zanoon.model.Electron;
+import src.main.java.com.zanoon.model.Nucleus;
+import src.main.java.com.zanoon.model.Particle;
+import src.main.java.com.zanoon.model.Nucleon.Neutron;
+import src.main.java.com.zanoon.model.Nucleon.Proton;
 
 public class SimulationPanel extends JPanel {
 
     private Electron electron;
     private Electron electron2;
 
-    private double R = 150; // Initial orbital radius
+    private int R = 150; // Initial orbital radius
     private double attraction = 0.88; // attraction magnitude force
     private double v = Math.sqrt(attraction * R);
+    private int maxRadius = 1;
 
     // center of orbit (nucleus)
     private final double cx = 200;
     private final double cy = 200;
+
+    private Neutron neutron;
+    private Proton proton;
+    private Nucleus nucleus;
 
     public SimulationPanel() {
 
         electron = new Electron(cx + R, cy, 0, v, 1, 0, 8);
         electron2 = new Electron(cx, cy + R, -v, 0, 1, Math.PI, 8);
 
+        proton = new Proton(0, 0, 0, 0, 1, 0, 8);
+        neutron = new Neutron(0, 0, 0, 0, 1, 0, 8);
+
+        nucleus = new Nucleus(cx, cy);
+        
+        nucleus.addParticle(proton);
+        nucleus.addParticle(neutron);
+
+        nucleus.organicCluster(maxRadius); // This is done, to prevent all nucleus particles starting off stacked, which will prevent performance drain
+
         // animation timer (~60 FPS)
         Timer timer = new Timer(16, e -> {
+            nucleus.organicCluster(maxRadius);
             electron.orbit(cx, cy, attraction);
             electron2.orbit(cx, cy, attraction);
             repaint();
@@ -52,13 +72,29 @@ public class SimulationPanel extends JPanel {
             (int) (2 * electron.getR())
         );
     
-            // draw electron
-        g.setColor(Color.GREEN);
+            // draw electron2
+        g.setColor(Color.CYAN);
         g.fillOval(
             (int) (electron2.getX() - electron2.getR()),
             (int) (electron2.getY() - electron2.getR()),
             (int) (2 * electron2.getR()),
             (int) (2 * electron2.getR())
         );
+
+        // Drawing nucleus particles
+        for (Particle p : nucleus.getParticles()) {
+            if (p instanceof Proton) {
+                g.setColor(Color.RED);
+            } else {
+                g.setColor(Color.GRAY);
+            }
+
+            g.fillOval(
+                (int)(p.getX() - p.getR()),
+                (int)(p.getY() - p.getR()),
+                (int)(2 * p.getR()),
+                (int)(2 * p.getR())
+            );
+        }
     }
 }
