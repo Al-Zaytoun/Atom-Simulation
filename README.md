5944ff31ba25da02928014b2243bef059cda1866
Al-Zaytoun <zanoonhassan6@gmail.com>
2026-01-07 08:08:08 -0700

    Continue working towards a shader-based OpenGL approach

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Work in progress of an Atom Simulation project.
+
--- src/main/java/com/zanoon/render/AtomSimulation3D.java
+++ src/main/java/com/zanoon/render/AtomSimulation3D.java
@@ -2,12 +2,44 @@ package com.zanoon.render;
 
 import com.jogamp.opengl.*;
 import com.jogamp.opengl.awt.GLCanvas;
-import com.jogamp.opengl.util.FPSAnimator;
-import com.jogamp.opengl.glu.GLU;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
 
-public class AtomSimulation3D {
+
+public class AtomSimulation3D implements GLEventListener{
     
+    private GLCanvas glCanvas;
+
+    
+    public AtomSimulation3D() {
+        final GLProfile profile = GLProfile.get(GLProfile.GL4); // Version GL4
+        GLCapabilities capabilities = new GLCapabilities(profile);
+
+        glCanvas = new GLCanvas(capabilities);
+        glCanvas.addGLEventListener(this);
+
+
+    }
+
+    @Override
+    public void init(GLAutoDrawable drawable) {
+
+        // Retrieve the GL Object from the drawable
+        GL gl = drawable.getGL().getGL2();
+    }
+
+    @Override
+    public void display(GLAutoDrawable drawable) {
+        // rendering code here
+    }
+
+    @Override
+    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+        // reshape code here
+    }
+
+    @Override
+    public void dispose(GLAutoDrawable drawable) {
+        // resource cleanup code here
+    }
+
+
 }
